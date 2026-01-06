9c785c87c75046ad36acec77c053f52c5528eaa0
Olayinka dev <olayinkavideocreator@gmail.com>
2026-01-06 09:35:57 -0800

    Create GameToken.sol
    Add GameToken smart contract

---
+++ contract/GameToken.sol
@@ -0,0 +1,24 @@
+contract/GameToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GameToken is ERC20, Ownable {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 initialSupply_
+    ) ERC20(name_, symbol_) {
+        mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
